a8aa6362c3dcf9ba945f37e47b5bdb913e0faeab
Katherine Walden <katie.walden@hotmail.com>
2022-04-06 18:04:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+import plotly.express as px
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.graph_objs as go
-import pandas as pd
+from dash.dependencies import Input, Output, State
+from dash import html, dcc
+import dash_bootstrap_components as dbc
 
 ##### Initiate the app
 
