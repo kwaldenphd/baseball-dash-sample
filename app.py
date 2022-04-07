a7694679181b548ce047aa8af4465a79181acea7
Katherine Walden <katie.walden@hotmail.com>
2022-04-06 23:02:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,6 @@ schedule['Datetime'] = pd.to_datetime(schedule['Standardized_Date'])
 # make new datatime column the index
 schedule.set_index(['Datetime'], inplace=True)
 
-# subset data for games after 1938
-subset = schedule[schedule['Season'] >= 1938]
-
 # set colors/style
 colors = {
     'background': '#0c2340',
@@ -32,19 +29,19 @@ colors = {
 }
 
 # setup dropdown for game type
-type_dropdown = dcc.Dropdown(options=subset['Game_Type'].unique(), value='Home')
+type_dropdown = dcc.Dropdown(options=schedule['Standardized_Opponent'].sort_values(ascending=True).unique(), value='Navy')
 
 # setup layout
 app.layout = html.Div(
     html.Div(
         children = [
             html.H1("Explore Notre Dame Football Schedules", style={'color':colors['background'], 'font':'Arial'}),
-            html.Label('Choose Game Type:'),
+            html.Label('Choose Opponent:'),
             type_dropdown,
             dcc.Graph(id = 'schedule-points'),
-            html.A('Code on GitHub', href="https://github.com/kwaldenphd/sample-dash-app", style={'color':colors['green']}),
+            html.A('Code on GitHub', href="https://github.com/kwaldenphd/sample-dash-app", style={'color':colors['text']}),
             html.Br(),
-            html.A('Data Source', href="https://github.com/kwaldenphd/football-structured-data/blob/main/background.md#football-schedules", style={'color':colors['green']})
+            html.A('Data Source', href="https://github.com/kwaldenphd/football-structured-data/blob/main/background.md#football-schedules", style={'color':colors['text']})
         ]))
 
 # set up callback for interactivity
@@ -54,9 +51,9 @@ app.layout = html.Div(
 )
 
 # setup function to generate plot
-def update_graph(game_type):
-  subset2 = subset[subset['Game_Type'] == game_type]
-  bar_fig = px.bar(subset2, x='Season', y='Pts', color='Conf', title=f'Number of Points Over Time For {game_type} Games', hover_name = 'Standardized_Date', hover_data=['Standardized_Opponent'])
+def update_graph(opponent):
+  subset = schedule[schedule['Standardized_Opponent'] == opponent]
+  bar_fig = px.bar(subset, x='Season', y='Pts', color='Game_Type', color_discrete_map={'Home': '#00843d', 'Neutral': '#c99700', 'Away': '#0c2340'}, title=f'Number of Points Over Time For Games Played Versus {opponent}', hover_name = 'Standardized_Date', hover_data=['Combined_Location'])
   return bar_fig
 
 ##### run app
